fe2d536904e32164cd64d1441f301ed9c03af7a6
Pallab Dey <pallabdey21@gmail.com>
2026-03-02 23:30:33 +0600

    docs: fix wordings

---
+++ Queries/avg_pizza_per_order.sql
@@ -0,0 +1,4 @@
+select * from pizza_sales
+
+select cast(cast(sum(quantity) as decimal(10,2)) / 
+cast(count(distinct order_id) as decimal(10,2)) as decimal(10,2)) as avg_pizza_per_order from pizza_sales
